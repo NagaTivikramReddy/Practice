cb9ef5e861dea333e09bc73518342281654602bf
NagaTrivikramReddy777 <75057586+NagaTrivikramReddy777@users.noreply.github.com>
2021-06-26 21:40:40 +0530

    python file

--- dhoni.py
+++ dhoni.py
@@ -7,5 +7,3 @@ if __name__:"__main__":
     #the variables or function which are declared here are only accessible to this file and other files which import this file dont have access to thi
 
     password = 'admin'
-
- 
